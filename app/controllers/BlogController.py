f4d92fb56733a4243abc82e342f11e4020770a40
Muhammad Haziq <beinghaziq@gmail.com>
2023-11-27 18:21:02 +0500

    blog serializer added

--- app/controllers/BlogController.py
+++ app/controllers/BlogController.py
@@ -1,55 +1,61 @@
 # app/blog.py
-from fastapi import APIRouter, status, Response, HTTPException
+from typing import List
+from fastapi import APIRouter, status, HTTPException
 from app.pydantic_objects.blog import Blog as BlogBase
 from app.controllers.BaseController import Session, get_db, Depends
 from app.models.blog import Blog
+from app.serializers.BlogSerializer import BlogSerializer
 
 router = APIRouter()
 
-@router.get('/')
+@router.get('/', response_model = List[BlogSerializer])
 def index(db: Session = Depends(get_db)):
-    blogs = db.query(Blog).all()
-    return blogs
+	blogs = db.query(Blog).all()
+	return blogs
+	
 
-@router.post('/', status_code = status.HTTP_201_CREATED)
+@router.post('/', status_code = status.HTTP_201_CREATED, response_model = BlogSerializer)
 def create(blog: BlogBase, db: Session = Depends(get_db)):
-    new_blog = Blog(title = blog.title, body = blog.body)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
-    
-@router.get('/{id}', status_code = 200)
-def show(id, response: Response, db: Session = Depends(get_db)):
-    blog = db.query(Blog).filter(Blog.id == id).first()
+	new_blog = Blog(title = blog.title, body = blog.body)
+	db.add(new_blog)
+	db.commit()
+	db.refresh(new_blog)
+	return new_blog
 
-    if not blog:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Blog with {id} not found")
-    return blog
+
+@router.get('/{id}', status_code = 200, response_model = BlogSerializer)
+def show(id, db: Session = Depends(get_db)):
+	blog = db.query(Blog).filter(Blog.id == id).first()
+
+	if not blog:
+		raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Blog with {id} not found")
+	return blog
+
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id: int, db: Session = Depends(get_db)):
-    blog = db.query(Blog).filter(Blog.id == id).first()
+  blog = db.query(Blog).filter(Blog.id == id).first()
 
-    if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
+  if not blog:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
 
-    db.delete(blog)
-    db.commit()
+  db.delete(blog)
+  db.commit()
     
-    return { "message": "Blog deleted successfully" }
+  return { "message": "Blog deleted successfully" }
 
-@router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
+
+@router.put('/{id}', status_code=status.HTTP_202_ACCEPTED, response_model = BlogSerializer)
 def update(id: int, blog: BlogBase, db: Session = Depends(get_db)):
-    print(id, "lsjdiofoadsjaldio")
-    blog_to_update = db.query(Blog).filter(Blog.id == id).first()
-    
-    if not blog_to_update:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found") 
+	blog_to_update = db.query(Blog).filter(Blog.id == id).first()
+	
+	if not blog_to_update:
+		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = "Blog not found") 
 
-    for key, value in blog.dict().items():
-        setattr(blog_to_update, key, value)
+	for key, value in blog.dict().items():
+		setattr(blog_to_update, key, value)
 
-    db.commit()
-    db.refresh(blog_to_update)
-    return {'message': 'Blog updated successfully', 'blog': blog_to_update}
+	db.commit()
+	db.refresh(blog_to_update)
+	
+	return blog_to_update
---
+++ app/serializers/BlogSerializer.py
@@ -0,0 +1,5 @@
+from app.pydantic_objects.blog import Blog
+
+
+class BlogSerializer(Blog):
+    pass
