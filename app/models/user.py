e7c7fd55ad2deb5a0b63045742f9dc77c4a6f76e
Muhammad Haziq <beinghaziq@gmail.com>
2023-11-29 01:17:20 +0500

    User Model: indentation set

--- app/models/user.py
+++ app/models/user.py
@@ -3,9 +3,9 @@ from sqlalchemy import Column, Integer, String
 from database import Base
 
 class User(Base):
-	__tablename__ = 'users'
+  __tablename__ = 'users'
 
-	id = Column(Integer, primary_key=True, index=True)
-	email = Column(String)
-	name = Column(String)
-	password = Column(String)
+  id = Column(Integer, primary_key=True, index=True)
+  email = Column(String)
+  name = Column(String)
+  password = Column(String)
