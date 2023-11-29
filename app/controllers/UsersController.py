864875bb7d964a84e7bd355425881c7a77d00cfe
Muhammad Haziq <72576839+beinghaziq@users.noreply.github.com>
2023-11-29 15:57:25 +0500

    user creation added and password hashing added (#4)
    * user creation added and password hashing added
    
    * indentation set
    
    * User Model: indentation set
    
    * User Serializer: indentation set
    
    * User Controller: unused imports removed
    
    * spaces set

---
+++ app/controllers/UsersController.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, status
+from app.pydantic_objects.user import User as UserBase
+from app.controllers.BaseController import Session, get_db, Depends
+from app.models.user import User
+from app.serializers.UserSerializer import UserSerializer
+from app.services.text_transformer import TextTransformer
+
+router = APIRouter()
+
+@router.post('/', status_code = status.HTTP_201_CREATED, response_model=UserSerializer)
+def create(user: UserBase, db: Session = Depends(get_db)):
+  new_user = User(name = user.name, email = user.email, password = TextTransformer(user.password).encrypt())
+  db.add(new_user)
+  db.commit()
+  db.refresh(new_user)
+  return new_user
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.controllers.BlogController import router as blog_router
+from app.controllers.UsersController import router as user_router
 from database import engine, Base
 from app.models.blog import Blog
 
@@ -7,3 +8,4 @@ app = FastAPI()
 
 Base.metadata.create_all(engine)
 app.include_router(blog_router, prefix='/blog', tags=['blog'])
+app.include_router(user_router, prefix='/user', tags=['user'])
