571cd49ad39cb9e94b81d3afe5f81fb07d7966a8
Muhammad Haziq <beinghaziq@gmail.com>
2023-12-11 15:58:09 +0500

    crud methods moved to repository

--- app/repositories/blog.py
+++ app/repositories/blog.py
@@ -6,26 +6,27 @@ def all(db=Session):
 	blogs = db.query(Blog).all()
 	return blogs
 
-def create(blog):
-	new_blog = Blog(title = blog.title, body = blog.body, user_id = 1)
+def create(blog, db=Session):
+	new_blog = Blog(**blog.__dict__, user_id = 1)
 	db.add(new_blog)
 	db.commit()
 	db.refresh(new_blog)
 	return new_blog
 
-def find(id):
+def find(id, db=Session):
 	blog = db.query(Blog).filter(Blog.id == id).first()
 
 	if not blog:
 		raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Blog with {id} not found")
+	return blog
 
-def destroy(id):
-	blog = find(id)
+def destroy(id, db):
+	blog = find(id, db)
 	db.delete(blog)
 	db.commit()
 
-def update(blog):
-	blog_to_update = find(id)
+def update(id, blog, db):
+	blog_to_update = find(id, db)
 	for key, value in blog.dict().items():
 		setattr(blog_to_update, key, value)
 
