59ac7add42875ec8e92ce38308cf13d6316d8f6b
Muhammad Haziq <72576839+beinghaziq@users.noreply.github.com>
2023-12-12 16:40:56 +0500

    Repositories added (#8)
    * blog and user relation defined
    
    * blog created with user
    
    * custom methods added in serializers
    
    * indentation set
    
    * new controller added for handling user's blogs
    
    * indentationn set
    
    * repository added for reterieveing all records
    
    * crud methods moved to repository
    
    * endline added
    
    * spaces removed

--- app/controllers/BlogController.py
+++ app/controllers/BlogController.py
@@ -4,57 +4,30 @@ from fastapi import APIRouter, status, HTTPException
 from app.pydantic_objects.blog import Blog as BlogBase
 from app.controllers.BaseController import Session, get_db, Depends
 from app.models.blog import Blog
+import app.repositories.blog as BlogRepo
 from app.serializers.BlogSerializer import BlogSerializer
 
 router = APIRouter()
 
 @router.get('/', response_model = List[BlogSerializer])
 def index(db: Session = Depends(get_db)):
-	blogs = db.query(Blog).all()
-	return blogs
+	return BlogRepo.all(db)
 	
 
 @router.post('/', status_code = status.HTTP_201_CREATED, response_model = BlogSerializer)
 def create(blog: BlogBase, db: Session = Depends(get_db)):
-	new_blog = Blog(title = blog.title, body = blog.body, user_id = 1)
-	db.add(new_blog)
-	db.commit()
-	db.refresh(new_blog)
-	return new_blog
-
+	return BlogRepo.create(blog, db)
 
 @router.get('/{id}', status_code = 200, response_model = BlogSerializer)
 def show(id, db: Session = Depends(get_db)):
-	blog = db.query(Blog).filter(Blog.id == id).first()
-
-	if not blog:
-		raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Blog with {id} not found")
-	return blog
+	return BlogRepo.find(id, db)
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id: int, db: Session = Depends(get_db)):
-  blog = db.query(Blog).filter(Blog.id == id).first()
-
-  if not blog:
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
-
-  db.delete(blog)
-  db.commit()
-    
+  BlogRepo.destroy(id, db)
   return { "message": "Blog deleted successfully" }
 
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED, response_model = BlogSerializer)
 def update(id: int, blog: BlogBase, db: Session = Depends(get_db)):
-	blog_to_update = db.query(Blog).filter(Blog.id == id).first()
-	
-	if not blog_to_update:
-		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = "Blog not found") 
-
-	for key, value in blog.dict().items():
-		setattr(blog_to_update, key, value)
-
-	db.commit()
-	db.refresh(blog_to_update)
-	
-	return blog_to_update
+	return BlogRepo.update(id, blog, db)
