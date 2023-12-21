373b030a3f54e0eb942bb87421ef3d53886efc54
Muhammad Haziq <beinghaziq@gmail.com>
2023-12-21 19:17:38 +0500

    jwt creation added

--- app/controllers/sessions_controller.py
+++ app/controllers/sessions_controller.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, status, HTTPException
 from app.pydantic_objects.login import Login
 from app.repositories.user_repository import UserRepository
 from app.services.text_transformer import TextTransformer
+from app.services import jwt
 
 router = APIRouter()
 user_repo = UserRepository()
@@ -11,4 +12,6 @@ def create(request: Login):
 	user = user_repo.find_by('email', request.username)
 	if not TextTransformer(request.password, user.password).verify():
 		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"password incorrect")
-	return user
+	
+	token = jwt.create_access_token(data={ 'sub': user.email })
+	return { 'access_token': token, 'token_type': 'bearer' }
