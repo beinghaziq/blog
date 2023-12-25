d6e8bc9bbcc15fad9ca785ce08a534db257e6fd4
Muhammad Haziq <beinghaziq@gmail.com>
2023-12-25 22:40:57 +0500

    get_current_user method added

--- app/controllers/BlogController.py
+++ app/controllers/BlogController.py
@@ -1,15 +1,17 @@
 # app/blog.py
 from typing import List # Info: Typing is by-default included in python 3.10+
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends
 from app.pydantic_objects.blog import Blog as BlogBase
 from app.repositories.blog_repository import BlogRepository as BlogRepo
 from app.serializers.BlogSerializer import BlogSerializer
+from app.controllers.helpers import oauth2
 
 router = APIRouter()
 blog_repo = BlogRepo()
 
-@router.get('/', response_model = List[BlogSerializer])
-def index():
+
+@router.get('/', response_model=List[BlogSerializer])
+def index(current_user: str = Depends(oauth2.get_current_user)):
 	return blog_repo.all()
 	
 @router.post('/', status_code = status.HTTP_201_CREATED, response_model = BlogSerializer)
