715a36c576304dd4ce42f2e216d9bea202bcb3db
Muhammad Haziq <72576839+beinghaziq@users.noreply.github.com>
2024-02-06 16:49:22 +0500

    Console added for python (#12)
    * cors setup
    
    * console file added for project

--- Readme.md
+++ Readme.md
@@ -3,3 +3,6 @@ pip3 install -r requirements.txt
 python -m uvicorn main:app --reload
 
 create blog.db in parent directory
+
+# To Run console and run queries
+python3 console.py 
--- app/models/blog.py
+++ app/models/blog.py
@@ -13,3 +13,7 @@ class Blog(Base):
 	creator = relationship('User', back_populates = 'blogs')
 	created_at = Column(DateTime(timezone=True), server_default=func.now())
 	updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
+	def __repr__(self):
+		attributes = ', '.join(f"{key}={repr(value)}" for key, value in vars(self).items())
+		return f"{self.__class__.__name__}({attributes})"
